8674e46cf22b01f4f4e01cdd2ac289cbf4ba90d3
Ganesh Surna <133744396+8247783396@users.noreply.github.com>
2023-06-08 11:28:24 +0530

    Add files via upload

--- Anagram.py
+++ Anagram.py
@@ -1,11 +1,30 @@
-a=input().split()
-a=set(a)
-d={}
-for i in a:
-    s=sorted(i)
-    k="".join(s)
-    if k not in d:
-        d[k]=[i]
+""" Anagram Grouping
+
+You are given a list of words. Your task is to group the words into sets of anagrams. Anagrams are words that have the same characters but in a different order.
+
+Write a function that takes the list of words as input and returns a list of sets, where each set contains words that are anagrams of each other.
+
+For example, given the following list of words: ["cat", "dog", "tac", "god", "act"], the function should return: [["cat", "tac", "act"], ["dog", "god"]].
+
+"""
+
+
+words = input()
+words = words.split()
+words = set(words)
+
+d = {}
+
+for word in words:
+
+    sorted_word = sorted(word)
+    joined_sorted_word = "".join(sorted_word)
+
+    if joined_sorted_word not in d:
+        d[joined_sorted_word] = [word]
+        
     else:
-        d[k].append(i)
-print(list(d.values()))
+        d[joined_sorted_word].append(word)
+
+values = list(d.values())
+print(values)
